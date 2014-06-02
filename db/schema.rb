c0fab69755a55e1382018a8e27c62d03447e1afd
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2014-06-02 14:04:27 +0200

    try pg as db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140602102016) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "genres", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
